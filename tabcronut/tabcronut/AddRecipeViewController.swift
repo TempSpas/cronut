f628a741893191f53070993cc8af75a636a3497d
TempSpas <nicoverbeek@erratics-166.dynamic.rpi.edu>
2016-10-23 15:33:44 -0400

    Add tabcronut project files to the main directory

---
+++ tabcronut/tabcronut/AddRecipeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AddRecipeViewController.swift
+//  tabcronut
+//
+//  Created by Nico Verbeek on 10/23/16.
+//  Copyright © 2016 Cronut LLC. All rights reserved.
+//
+
+import UIKit
+
+class AddRecipeViewController: UIViewController {
+
+    // MARK: Properties
+    @IBOutlet weak var recipeTitle: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    /*
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
