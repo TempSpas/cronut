4ad9ef7f0394774aee497c34c0cf4c3d262236a7
Aditi Nataraj <adtnataraj@gmail.com>
2016-10-25 14:15:49 -0400

    still trying to get table

---
+++ tabcronut/tabcronut/RecipeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RecipeTableViewCell.swift
+//  tabcronut
+//
+//  Created by Aditi Nataraj on 10/24/16.
+//  Copyright © 2016 Cronut LLC. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: RecipeTableViewController {
+
+    @IBOutlet weak var RecipeName: UIView!
+    //RecipeName.text = "testing"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    /*func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }*/
+
+}
