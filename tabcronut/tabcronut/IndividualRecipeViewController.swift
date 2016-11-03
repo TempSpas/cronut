a1801670f1b010c6f882f46372f3f59d48167b7f
Ken Liu <liukrimhim@gmail.com>
2016-11-02 22:17:41 -0400

    readytogo

--- tabcronut/tabcronut/IndividualRecipeViewController.swift
+++ tabcronut/tabcronut/IndividualRecipeViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import EventKit
 
 class IndividualRecipeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var recipeTitle: String = ""
+    var savedEventId : String = ""
     
     @IBOutlet weak var ingrTable: UITableView!
     @IBOutlet weak var dirTable: UITableView!
@@ -101,7 +103,138 @@ class IndividualRecipeViewController: UIViewController, UITableViewDelegate, UIT
         return cell1!
     }
     
-
+    
+    // Creates an event in the EKEventStore. The method assumes the eventStore is created and
+    // accessible
+    func createEvent(_ eventStore: EKEventStore, title: String, startDate: Date, endDate: Date) {
+        
+        let alertController = UIAlertController(title: "Add Reminder", message: "Fill the form and take a seat! Next!", preferredStyle: .alert)
+        
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel) { (action:UIAlertAction) in
+            //This is called when the user presses the cancel button.
+            print("You've pressed the cancel button");
+        }
+        
+        let actionOK = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+            //This is called when the user presses the login button.
+            let textTitle = alertController.textFields![0] as UITextField;
+            let textStart = alertController.textFields![1] as UITextField;
+            let textEnd = alertController.textFields![2] as UITextField;
+            
+            
+            let event = EKEvent(eventStore: eventStore)
+            event.title = textTitle.text!
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yyyy hh:mm a"
+            
+            //print(dateFormatter.string(from: dateFormatter.date( from: "11/02/2016 13:00" )!))
+            
+            //event.startDate = startDate
+            //event.endDate = endDate
+            event.startDate = dateFormatter.date( from: textStart.text! )!
+            event.endDate = dateFormatter.date( from: textEnd.text! )!
+            event.calendar = eventStore.defaultCalendarForNewEvents
+            do {
+                try eventStore.save(event, span: .thisEvent)
+                self.savedEventId = event.eventIdentifier
+            } catch {
+                print("Bad things happened")
+            }
+            
+            let alertController = UIAlertController(title: "Just to let you know", message: "Reminder was added successfully, yay!", preferredStyle: .alert)
+            
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+                print("You've pressed OK button");
+            }
+            
+            alertController.addAction(OKAction)
+            self.present(alertController, animated: true, completion:nil)
+            
+            //print("The user entered:%@ & %@",textUser.text!,textPW.text!);
+        }
+        
+        alertController.addTextField { (textField) -> Void in
+            //Configure the attributes of the first text box.
+            textField.placeholder = "Event Title"
+        }
+        
+        alertController.addTextField { (textField) -> Void in
+            //Configure the attributes of the second text box.
+            textField.placeholder = "MM/dd/yyyy hh:mm AM(PM)"
+        }
+        
+        alertController.addTextField { (textField) -> Void in
+            //Configure the attributes of the third text box.
+            textField.placeholder = "MM/dd/yyyy hh:mm AM(PM)"
+        }
+        
+        //Add the buttons
+        alertController.addAction(actionCancel)
+        alertController.addAction(actionOK)
+        
+        //Present the alert controller
+        self.present(alertController, animated: true, completion:nil)
+    }
+    
+    // Removes an event from the EKEventStore. The method assumes the eventStore is created and
+    // accessible
+    func deleteEvent(_ eventStore: EKEventStore, eventIdentifier: String) {
+        let eventToRemove = eventStore.event(withIdentifier: eventIdentifier)
+        if (eventToRemove != nil) {
+            do {
+                try eventStore.remove(eventToRemove!, span: .thisEvent)
+            } catch {
+                print("Bad things happened")
+            }
+        }
+    }
+    
+    // Responds to button to add event. This checks that we have permission first, before adding the
+    // event
+    @IBAction func addEvent(_ sender: UIButton) {
+        let eventStore = EKEventStore()
+        
+        let startDate = Date()
+        let endDate = startDate.addingTimeInterval(60 * 60) // One hour
+        
+        
+        
+        if (EKEventStore.authorizationStatus(for: .event) != EKAuthorizationStatus.authorized) {
+            eventStore.requestAccess(to: .event, completion: {
+                granted, error in
+                self.createEvent(eventStore, title: "Test Event", startDate: startDate, endDate: endDate)
+            })
+        } else {
+            createEvent(eventStore, title: "Test Event", startDate: startDate, endDate: endDate)
+        }
+        
+    }
+    
+    
+    // Responds to button to remove event. This checks that we have permission first, before removing the
+    // event
+    @IBAction func removeEvent(_ sender: UIButton) {
+        let eventStore = EKEventStore()
+        
+        if (EKEventStore.authorizationStatus(for: .event) != EKAuthorizationStatus.authorized) {
+            eventStore.requestAccess(to: .event, completion: { (granted, error) -> Void in
+                self.deleteEvent(eventStore, eventIdentifier: self.savedEventId)
+            })
+        } else {
+            deleteEvent(eventStore, eventIdentifier: savedEventId)
+        }
+        
+        let alertController = UIAlertController(title: "Just to let you know", message: "Reminder was removed successfully, yay!", preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+            print("You've pressed OK button");
+        }
+        
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion:nil)
+        
+    }
     
     // MARK: - Navigation
 
