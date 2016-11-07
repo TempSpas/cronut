fcc0e78bc2fa36d1bcf9ee1a540fd5814973fe82
smithr1924 <smithr1924@gmail.com>
2016-11-07 18:16:37 -0500

    Hopefully fixed the scaling function for swift 3

--- tabcronut/tabcronut/scanner.swift
+++ tabcronut/tabcronut/scanner.swift
@@ -1,17 +1,33 @@
 import UIKit
 import Foundation
 
-class func scaleUIImageToSize(let image: UIImage, let size: CGSize) -> UIImage {
-    let hasAlpha = false
-    let scale: CGFloat = 0.0 // Automatically use scale factor of main screen
+extension UIImage {
+    /// Scales an image to fit within a bounds with a size governed by the passed size. Also keeps the aspect ratio.
+    /// Switch MIN to MAX for aspect fill instead of fit.
+    ///
+    /// - parameter newSize: newSize the size of the bounds the image must fit within.
+    ///
+    /// - returns: a new scaled image.
+    func scaleImageToSize(newSize: CGSize) -> UIImage {
+        var scaledImageRect = CGRect.zero
 
-    UIGraphicsBeginImageContextWithOptions(size, !hasAlpha, scale)
-    image.drawInRect(CGRect(origin: CGPointZero, size: size))
+        let aspectWidth = newSize.width/size.width
+        let aspectheight = newSize.height/size.height
 
-    let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
+        let aspectRatio = max(aspectWidth, aspectheight)
 
-    return scaledImage
+        scaledImageRect.size.width = size.width * aspectRatio;
+        scaledImageRect.size.height = size.height * aspectRatio;
+        scaledImageRect.origin.x = (newSize.width - scaledImageRect.size.width) / 2.0;
+        scaledImageRect.origin.y = (newSize.height - scaledImageRect.size.height) / 2.0;
+
+        UIGraphicsBeginImageContext(newSize)
+        draw(in: scaledImageRect)
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return scaledImage!
+    }
 }
 
 //class Scanner 
